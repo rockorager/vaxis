b1d9fde27f6eaffb4b6b72b06ae671b57a085ccc
Tim Culverhouse <tim@timculverhouse.com>
2023-09-02 08:40:27 -0500

    text: fix example
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- text_test.go
+++ text_test.go
@@ -3,13 +3,13 @@ package vaxis_test
 import "git.sr.ht/~rockorager/vaxis"
 
 func ExampleText() {
+	vx, _ := vaxis.New(vaxis.Options{})
 	c := vaxis.Text{
 		Content:    "a",
 		Foreground: vaxis.IndexColor(1),
 		Attribute:  vaxis.AttrBold | vaxis.AttrBlink,
 	}
-	win := vaxis.Window{}
 
 	// Fills the entire window with blinking, bold, red "a"s
-	win.Fill(c)
+	vx.Window().Fill(c)
 }
