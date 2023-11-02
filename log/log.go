8e00a4b3f93337c8d1f1e6ae32cb912d002ce4e8
Tim Culverhouse <tim@timculverhouse.com>
2023-11-02 10:02:41 -0500

    log: simplify logging
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- log/log.go
+++ log/log.go
@@ -4,162 +4,93 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"sync"
 	"time"
 )
 
 const (
-	LevelError = iota
+	LevelError int = iota
 	LevelWarn
 	LevelInfo
 	LevelDebug
 	LevelTrace
 
-	trace = "TRACE"
-	debug = "DEBUG"
-	info  = "INFO"
-	warn  = "WARN"
-	err   = "ERROR"
-
-	// formats
-	// prefix + time
-	basic = "%-5s %s "
-	color = "\x1b[3%dm%-5s\x1b[m \x1b[2m%s\x1b[m "
+	calldepth = 3
+	flags     = log.Lshortfile
 )
 
-type logger struct {
-	l  *log.Logger
-	mu sync.Mutex
-
-	level int
-	fmt   string
-	color bool
-}
-
-var l = &logger{
-	l:     log.New(io.Discard, "", 0),
-	color: true,
-	fmt:   "15:04:05.000",
-}
+var (
+	level       = LevelError
+	traceLogger = log.New(io.Discard, "TRACE ", flags)
+	debugLogger = log.New(io.Discard, "DEBUG ", flags)
+	infoLogger  = log.New(io.Discard, "INFO  ", flags)
+	warnLogger  = log.New(io.Discard, "WARN  ", flags)
+	errorLogger = log.New(io.Discard, "ERROR ", flags)
+)
 
 // LevelError = 0
 // LevelWarn = 1
 // LevelInfo  = 2
 // LevelDebug  = 3
 // LevelTrace = 4
-func SetLevel(level int) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.level = level
+func SetLevel(l int) {
+	level = l
 }
 
 func SetOutput(w io.Writer) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.l.SetOutput(w)
-}
-
-func SetTimeFormat(format string) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.fmt = format
-}
-
-func DisableColor() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.color = false
-}
-
-// Default
-func EnableColor() {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.color = true
+	traceLogger.SetOutput(w)
+	debugLogger.SetOutput(w)
+	infoLogger.SetOutput(w)
+	warnLogger.SetOutput(w)
+	errorLogger.SetOutput(w)
 }
 
 func now() string {
-	return time.Now().Format(l.fmt)
+	return time.Now().Format("15:04:05.000")
+}
+
+func fmtMessage(message string, args ...any) string {
+	if len(args) > 0 {
+		message = fmt.Sprintf(message, args...)
+	}
+	return fmt.Sprintf("%s %s", now(), message)
 }
 
 func Trace(format string, args ...any) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.level < LevelTrace {
+	if level < LevelTrace {
 		return
 	}
-	var prefix string
-	switch l.color {
-	case true:
-		prefix = fmt.Sprintf(color, 7, trace, now())
-	default:
-		prefix = fmt.Sprintf(basic, trace, now())
-	}
-	l.l.Printf(prefix+format, args...)
+	message := fmtMessage(format, args...)
+	traceLogger.Output(calldepth, message)
 }
 
 func Debug(format string, args ...any) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.level < LevelDebug {
+	if level < LevelDebug {
 		return
 	}
-	var prefix string
-	switch l.color {
-	case true:
-		prefix = fmt.Sprintf(color, 5, debug, now())
-	default:
-		prefix = fmt.Sprintf(basic, debug, now())
-	}
-	l.l.Printf(prefix+format, args...)
+	message := fmtMessage(format, args...)
+	debugLogger.Output(calldepth, message)
 }
 
 func Info(format string, args ...any) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.level < LevelInfo {
+	if level < LevelInfo {
 		return
 	}
-	var prefix string
-	switch l.color {
-	case true:
-		prefix = fmt.Sprintf(color, 4, info, now())
-	default:
-		prefix = fmt.Sprintf(basic, info, now())
-	}
-	l.l.Printf(prefix+format, args...)
+	message := fmtMessage(format, args...)
+	infoLogger.Output(calldepth, message)
 }
 
 func Warn(format string, args ...any) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.level < LevelWarn {
+	if level < LevelWarn {
 		return
 	}
-	var prefix string
-	switch l.color {
-	case true:
-		prefix = fmt.Sprintf(color, 3, warn, now())
-	default:
-		prefix = fmt.Sprintf(basic, warn, now())
-	}
-	l.l.Printf(prefix+format, args...)
+	message := fmtMessage(format, args...)
+	warnLogger.Output(calldepth, message)
 }
 
 func Error(format string, args ...any) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	if l.level < LevelError {
+	if level < LevelError {
 		return
 	}
-	if l.color {
-	}
-	var prefix string
-	switch l.color {
-	case true:
-		prefix = fmt.Sprintf(color, 1, err, now())
-	default:
-		prefix = fmt.Sprintf(basic, err, now())
-	}
-	l.l.Printf(prefix+format, args...)
+	message := fmtMessage(format, args...)
+	errorLogger.Output(calldepth, message)
 }
