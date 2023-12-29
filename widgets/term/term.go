26a2efb11639389d5046f2ef700f07c817fe756c
Tim Culverhouse <tim@timculverhouse.com>
2023-12-29 10:56:26 -0600

    widgets(term): clear dirty as early as possible
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- widgets/term/term.go
+++ widgets/term/term.go
@@ -461,7 +461,7 @@ func (vt *Model) Close() {
 func (vt *Model) Draw(win vaxis.Window) {
 	vt.mu.Lock()
 	defer vt.mu.Unlock()
-	defer atomicStore(&vt.dirty, false)
+	atomicStore(&vt.dirty, false)
 	width, height := win.Size()
 	if int(width) != vt.width() || int(height) != vt.height() {
 		win.Width = width
