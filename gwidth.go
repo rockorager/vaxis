066bbc5fe7c50cb891bde88a07557db740997404
Tim Culverhouse <tim@timculverhouse.com>
2024-02-07 20:43:56 -0600

    gwidth: special case Variation Selectors
    go-runewidth reports Variation Selectors as width 1, when they should be
    width 0. Special case these. Issue reported in go-runewidth #76
    
    Reference: https://github.com/mattn/go-runewidth/issues/76
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- gwidth.go
+++ gwidth.go
@@ -11,9 +11,12 @@ func gwidth(s string, unicode bool) int {
 	}
 	total := 0
 	for _, r := range s {
-		if r == 0xFE0F {
-			// runewidth incorrectly thinks a VS16 selector is 1
-			// wide
+		if r >= 0xFE00 && r <= 0xFE0F {
+			// Variation Selectors 1 - 16
+			continue
+		}
+		if r >= 0xE0100 && r <= 0xE01EF {
+			// Variation Selectors 17-256
 			continue
 		}
 		total += runewidth.RuneWidth(r)
