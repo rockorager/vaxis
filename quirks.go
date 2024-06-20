1a576acfa30c7aa291f0fcfb2accff08d8991219
Tim Culverhouse <tim@timculverhouse.com>
2024-06-20 14:34:05 -0500

    quirks: remove asciinema legacy sgr
    Asciinema now supports ':' delimiters
    
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- quirks.go
+++ quirks.go
@@ -15,13 +15,8 @@ func (vx *Vaxis) applyQuirks() {
 		vx.caps.noZWJ = true
 	}
 
-	// TODO: remove this when asciinema supports ':' delimiters in RGB
 	if os.Getenv("ASCIINEMA_REC") != "" {
-		fgIndexSet = strings.ReplaceAll(fgIndexSet, ":", ";")
-		fgRGBSet = strings.ReplaceAll(fgRGBSet, ":", ";")
-		bgIndexSet = strings.ReplaceAll(bgIndexSet, ":", ";")
-		bgRGBSet = strings.ReplaceAll(bgRGBSet, ":", ";")
-		// Asciinema also doesn't support any advanced image protocols
+		// Asciinema doesn't support any advanced image protocols
 		vx.graphicsProtocol = halfBlock
 	}
 	if os.Getenv("VAXIS_FORCE_LEGACY_SGR") != "" {
