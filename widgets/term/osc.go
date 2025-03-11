db91fb0aae943b5b9263230804d325520efc6563
Pavel Borzenkov <pavel@borzenkov.net>
2025-03-11 18:37:50 +0100

    term: don't respond with terminal colors to all OSC 11 queries
    OSC11 allows to both query and set background color. The programs
    that try to set the color, for example, 'fish' shell, don't expect
    the terminal to answer with the current background color. This results
    in 'fish' putting the response onto the command line.
    
    So only respond if OSC11 payload is '?', otherwise simply ignore the
    command.
    
    In future this can be extended to passthrough OSC11 to the underlying
    terminal to change its background color.

--- widgets/term/osc.go
+++ widgets/term/osc.go
@@ -27,15 +27,17 @@ func (vt *Model) osc(data string) {
 	case "9":
 		vt.postEvent(EventNotify{Body: val})
 	case "11":
-		if vt.vx == nil {
-			return
+		if val == "?" {
+			if vt.vx == nil {
+				return
+			}
+			rgb := vt.vx.QueryBackground().Params()
+			if len(rgb) == 0 {
+				return
+			}
+			resp := fmt.Sprintf("\x1b]11;rgb:%02x/%02x/%02x\x07", rgb[0], rgb[1], rgb[2])
+			vt.pty.WriteString(resp)
 		}
-		rgb := vt.vx.QueryBackground().Params()
-		if len(rgb) == 0 {
-			return
-		}
-		resp := fmt.Sprintf("\x1b]11;rgb:%02x/%02x/%02x\x07", rgb[0], rgb[1], rgb[2])
-		vt.pty.WriteString(resp)
 	case "52":
 		_, val, _ := cutString(val, ";")
 		decodedBytes, err := base64.StdEncoding.DecodeString(val)
