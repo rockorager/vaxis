02cbf97c910f28759429e76db60314756e22e851
Alex Vidal <alex@heyviddy.com>
2025-05-07 13:24:26 -0500

    vxfw(center): allow child to draw as small as possible

--- vxfw/center/center.go
+++ vxfw/center/center.go
@@ -18,14 +18,18 @@ func (c *Center) Draw(ctx vxfw.DrawContext) (vxfw.Surface, error) {
 	if ctx.Max.HasUnboundedHeight() || ctx.Max.HasUnboundedWidth() {
 		panic("Center must have bounded constraints")
 	}
-	chS, err := c.Child.Draw(ctx)
+
+	// Draw the child with no minimum so we can center it with any remaining space
+	chS, err := c.Child.Draw(ctx.WithMin(vxfw.Size{}))
 	if err != nil {
 		return vxfw.Surface{}, nil
 	}
+
 	// Create the surface for center
 	s := vxfw.NewSurface(ctx.Max.Width, ctx.Max.Height, c)
 	offX := (ctx.Max.Width - chS.Size.Width) / 2
 	offY := (ctx.Max.Height - chS.Size.Height) / 2
+
 	s.AddChild(int(offX), int(offY), chS)
 	return s, err
 }
